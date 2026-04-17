2d4d14bca04e08301130560c913bcd7123db0145
Alshimaa Ahmed Mohammed <AlshimaaAhmed@users.noreply.huggingface.co>
2026-04-17 19:45:45 +0000

    Update app/src/chat_schemas/response_schema.py

--- app/src/chat_schemas/response_schema.py
+++ app/src/chat_schemas/response_schema.py
@@ -9,18 +9,20 @@ class ChatResponse(BaseModel):
     content: str
     conversationId: str
     conversation_title:Optional[str]
-    clientMessageId: str = None,
-    role: str = 'ai',
+    clientMessageId: str = None
+    role: str = 'ai'
     is_idea_saved: bool = False
     is_full_idea: bool
     data: Optional[Dict] = None
+    inspired_by: Optional[List[str]] = None
+
 
 class ChatRequest(BaseModel):
     content: str
     conversationId: str
     isNewConversation: bool 
     clientMessageId: str = None
-    domain: Optional[List] = None
+    domain: Optional[str] = None
     data: Optional[Dict] = None
 
 
