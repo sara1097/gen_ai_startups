b3983dcbb10a3af221a88494f72392e9d3e65045
Alshimaa Ahmed Mohammed <AlshimaaAhmed@users.noreply.huggingface.co>
2026-04-17 19:56:18 +0000

    Update app/main.py

--- app/main.py
+++ app/main.py
@@ -9,21 +9,29 @@ load_dotenv(".env")
 
 from app.src.chat_schemas.response_schema import ChatRequest, ChatResponse
 from fastapi import FastAPI
+from fastapi import Request
 
 from app.src.engine.core.reasoning_router import route_reasoning
 
 app = FastAPI(title="Startup AI Service")
 
 @app.post("/chat", response_model=ChatResponse)
-def chat_endpoint(request: ChatRequest):
-    logger.debug(f"Request content: {request}")
+async def chat_endpoint(request: Request):
+    body = await request.json()
+
+    logger.info("📥 Raw request received")
+    logger.debug(f"Full body: {body}")
+
+    # بعد كده تحولي لـ schema
+    parsed_request = ChatRequest(**body)
+
     result = route_reasoning(
-        user_input=request.content,
-        data=request.data,
-        isNewConversation=request.isNewConversation,
-        clientMessageId = request.clientMessageId,
-        conversationId=request.conversationId,
-        domain=request.domain
+        user_input=parsed_request.content,
+        data=parsed_request.data,
+        isNewConversation=parsed_request.isNewConversation,
+        clientMessageId=parsed_request.clientMessageId,
+        conversationId=parsed_request.conversationId,
+        domain=parsed_request.domain
     )
 
     return result
